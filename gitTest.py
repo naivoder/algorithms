70cc352a41b5349482c8c58d6a7ee3caa300fa45
naivoder <credovia@students.kennesaw.edu>
2020-04-15 12:39:36 -0400

    test git package

---
+++ gitTest.py
@@ -0,0 +1,12 @@
+"""
+this file was created to test the atom ide git package, although I like to believe it makes the computer do pushups
+maybe you could even argue that it keeps the motherboard warm on a cold day? idk its a stretch...
+
+"""
+
+def excercise()
+    while True:
+        pass
+
+if __name__=="__main__":
+    excercise()
