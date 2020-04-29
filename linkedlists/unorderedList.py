c15737348b57374d8e67590dbf25f66467ef28c3
naivoder <credovia@students.kennesaw.edu>
2020-04-29 11:09:38 -0400

    def length method, modify head datastructure, add length check

--- linkedlists/unorderedList.py
+++ linkedlists/unorderedList.py
@@ -9,34 +9,39 @@ from node import Node
 class UnorderedList:
 
     def __init__(self):
-        self.head = None
+        self.head = [None, 0]
 
     def __str__(self):
         stuff = []
-        step = self.head
+        step = self.head[0]
         while step != None:
             stuff.append(step.content())
             step = step.reference()
         return str(stuff)
 
     def empty(self):
-        return self.head == None
+        return self.head[0] == None
 
     def add(self, item):
         hold = Node(item)
-        hold.point(self.head)
-        self.head = hold
+        hold.point(self.head[0])
+        self.head[0] = hold
+        self.head[1] += 1
 
     def size(self):
-        step = self.head
+        step = self.head[0]
         count = 0
         while step != None:
             count += 1
             step = step.reference()
         return count
 
+    def length(self):
+        count = self.head[1]
+        return count
+
     def search(self, item):
-        step = self.head
+        step = self.head[0]
         found = False
         while step != None and not found:
             if step.content() == item:
@@ -47,7 +52,7 @@ class UnorderedList:
 
     # to remove a node, we assign the pointer from the preceding node to the reference of the node we wish to skip
     def delete(self, item):
-        step = self.head
+        step = self.head[0]
         last = None
         found = False
         while not found:
@@ -58,16 +63,18 @@ class UnorderedList:
                 step = step.reference()
 
         if last == None:
-            self.head = step.reference()
+            self.head[0] = step.reference()
         else:
             last.point(step.reference())
+        self.head[1] -= 1
 
     def append(self, item):
-        step = self.head
+        step = self.head[0]
         hold = Node(item)
         while step.reference() != None:
             step = step.reference()
         step.point(hold)
+        self.head[1] += 1
 
 if __name__=="__main__":
     print("##---UNORDERED LINKED LIST---###")
@@ -94,3 +101,4 @@ if __name__=="__main__":
     print("Appending 15")
     print("Item '15' in list? ", list.search(15))
     print("Linked List:", list)
+    print("Length of list:", list.length())
