780c0aae94ac5bf117b77759b931b8ff281fac9b
naivoder <credovia@students.kennesaw.edu>
2020-05-07 08:41:36 -0400

    add bubbleTime comparison, add bubble sort methods, add selectionSort

---
+++ runtime/bubbleTime.py
@@ -0,0 +1,27 @@
+"""
+this file benchmarks two different versions of the bubble sort algorithm, just because I was curious.
+'ffi' stands for 'For-For-If'
+'wfi' stands for 'While-For-If'
+SPOILER... they're identical. Substituting a while loop has no noticable effect on runtime.
+the third version of the algorithm utilizes Python's simultaneous assignment property to reduce the number of steps, however it also shows no noticable improvement in runtime.
+
+"""
+import timeit, random
+from timeit import Timer
+from sorting.bubbleSort import *
+
+ffiData = [random.randint(0,100) for num in range(1000)]
+wfiData = ffiData[:]
+simData = ffiData[:]
+
+ffiBubble = Timer("bubble(ffiData)", "from __main__ import bubble, ffiData")
+wfiBubble = Timer("bubbleAlt(wfiData)", "from __main__ import bubbleAlt, wfiData")
+simBubble = Timer("bubbleSim(simData)", "from __main__ import bubbleSim, simData")
+
+ffiTime = ffiBubble.timeit(number=1000)
+wfiTime = wfiBubble.timeit(number=1000)
+simTime = simBubble.timeit(number=1000)
+
+print("FFI Bubble Sort Algorithm: %5.3fs" % ffiTime)
+print("WFI Bubble Sort Algorithm: %5.3fs" % wfiTime)
+print("Simul-Swap Bubble Sort Algorithm: %5.3fs" % simTime)
--- sorting/bubbleSort.py
+++ sorting/bubbleSort.py
@@ -1,8 +1,15 @@
 """
 this file implements the bubble sort algorithm, which makes multiple passes through a list comparing adjacent items and exchanging when necessary
-the algorithm requires n-1 passes to guarantee a sorted collection
+the algorithm requires n-1 passes to guarantee a sorted collection, O(n^2) runtime.
+the advantage to bubble sort is that a pass with no swaps indicates a sorted list, which might be a faster solution for collections with few items, demonstrated in the shortBubble function.
 
 """
+def bubbleSim(collection):
+    for loop in range(len(collection) - 1, 0, -1):
+        for index in range(loop):
+            if collection[index] > collection[index + 1]:
+                collection[index], collection[index + 1] = collection[index + 1], collection[index]
+    return collection
 
 def bubble(collection):
     for loop in range(len(collection) - 1, 0, -1):
@@ -12,9 +19,39 @@ def bubble(collection):
                 collection[index] = collection[index + 1]
                 collection[index + 1] = hold
     return collection
-    
+
+def bubbleAlt(collection):
+    loop = len(collection) -1
+    while loop > 0:
+        for index in range(loop):
+            if collection[index] > collection[index + 1]:
+                hold = collection[index]
+                collection[index] = collection[index + 1]
+                collection[index + 1] = hold
+        loop -= 1
+    return collection
+
+def shortBubble(collection):
+    exchange = True
+    loop = len(collection) - 1
+    while loop > 0 and exchange:
+        exchange = False
+        for index in range(loop):
+            if collection[index] > collection[index + 1]:
+                collection[index], collection[index + 1] = collection[index + 1], collection[index]
+
 if __name__=="__main__":
-    test = [86, 49, 13, 45, 39, 55, 94, 21, 39]
+    testA = [86, 49, 13, 45, 39, 55, 94, 21, 39]
+    testB = testA[:]; testC = testA[:]; testD = testA[:]
     print("###---Bubble Sort---###")
-    print("Test Data:", test)
-    print("Sorted Data:", bubble(test))
+    print("Test Data:", testA)
+    print("Sorted Data:", bubble(testA))
+    print("###---Bubble Sort (Alt)---###")
+    print("Test Data:", testB)
+    print("Sorted Data:", bubbleAlt(testB))
+    print("###---Bubble Sort (Simul-Swap)---###")
+    print("Test Data:", testC)
+    print("Sorted Data:", bubbleAlt(testC))
+    print("###---Bubble Sort (Short)---###")
+    print("Test Data:", testD)
+    print("Sorted Data:", bubbleAlt(testD))
