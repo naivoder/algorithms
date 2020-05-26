4f39f1007beff8977d2cc06f6b7d16b6fc1d0812
naivoder <credovia@students.kennesaw.edu>
2020-05-26 15:57:05 -0400

    add bit swap method

--- primitivetypes/same_weight.py
+++ primitivetypes/same_weight.py
@@ -17,29 +17,44 @@ def brute_force(word):
     for bit in bin(word):
         if bit == '1':
             weight += 1
-    found = False; i = 1
+    found = False; index = 1
     while not found:
-        next_up = word + i
+        next_up = word + index
         next_weight = 0
         for bit in bin(next_up):
             if bit == '1':
                 next_weight += 1
         if next_weight == weight:
             end = time.time() - start
-            print("%s seconds!")
+            print('%s seconds!' % end)
             return next_up
-        next_down = word - i
+        next_down = word - index
         next_weight = 0
         for bit in bin(next_down):
             if bit == '1':
                 next_weight += 1
         if next_weight == weight:
             end = time.time() - start
-            print("%s seconds!" % end)
+            print('%s seconds!' % end)
             return next_down
-        i += 1
+        index += 1
+    return found
 
-if __name__ == "__main__":
+def same_weight(word):
+    start = time.time()
+    num_bits = 64
+    for index in range(num_bits - 1):
+        if (word >> index) & 1 != (word >> (index + 1)) & 1:
+            word ^= (1 << index) | (1 << (index + 1))
+            end = time.time() - start
+            print('%s seconds!' % end)
+            return word
+
+if __name__=='__main__':
+    print('###---Swap Rightmost Different Bits---###')
+    print(same_weight(2**25))
     user = input('Do you want to run the brute force version? ')
     if user in ['y', 'Y']:
+        print('###---Brute Force Check Weights---###')
         print(brute_force(2**25))
+    print(same_weight(2**25))
